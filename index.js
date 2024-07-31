91db6b1b388dfa9ec908db72006fce46e008c7b8
Val H <val.hanovska@gmail.com>
2024-07-31 14:32:47 +0300

    naming rules

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 let box = 5;
-let variable;
-variable = 'hello'
+
+box = 10;
+
+box = 'text';
+
+box = true;
+
+let wrapper;
+wrapper = 'my name';
+
+let count = 7;
+
+console.log(typeof wrapper);
+
+console.log(typeof count);
+
+
+//Naming (називання) змінних 
+
+//Назва змінної має пояснювати для чого вона є або що всередині
+
+// let userInputValue;
+// let userName;
+// let userAvaterURL;
