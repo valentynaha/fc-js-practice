b1e1756132cc45d6f01b137d7d8bbcf29216f7e7
Val H <val.hanovska@gmail.com>
2024-08-09 15:39:09 +0300

    task resolve

--- index1.js
+++ index1.js
@@ -5,7 +5,7 @@ b = 4;
 console.log(a * b);
 
 let c;
-c = 20;
+c = 1000;
 let d;
 d = 10;
 console.log(c / d);
@@ -13,10 +13,21 @@ console.log(c / d);
 let num = 1;
 console.log(num);
 
-num += num;
+// num += num;
+++num;
+console.log(num);
+--num;
 console.log(num);
 
+// num -= num;
+// console.log(num);
+
+num +=22;
+console.log(num);
+num *=33;
+console.log(num);
+num/=44;
+console.log(num);
+num -=55;
 console.log(num);
 
-num -= num;
-console.log(num);
