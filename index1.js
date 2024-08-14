57d650d88c68e797e8914f45e290343d7ee37462
Val H <val.hanovska@gmail.com>
2024-08-14 13:59:12 +0300

    function task resolve

--- index1.js
+++ index1.js
@@ -1,33 +1,55 @@
-let a;
-a = 3;
-let b;
-b = 4;
-console.log(a * b);
 
-let c;
-c = 1000;
-let d;
-d = 10;
-console.log(c / d);
+// let a;
+// a = 3;
+// let b;
+// b = 4;
+// console.log(a * b);
 
-let num = 1;
-console.log(num);
+// let c;
+// c = 1000;
+// let d;
+// d = 10;
+// console.log(c / d);
 
-// num += num;
-++num;
-console.log(num);
---num;
-console.log(num);
-
-// num -= num;
+// let num = 1;
 // console.log(num);
 
-num +=22;
-console.log(num);
-num *=33;
-console.log(num);
-num/=44;
-console.log(num);
-num -=55;
-console.log(num);
+// // num += num;
+// ++num;
+// console.log(num);
+// --num;
+// console.log(num);
 
+// // num -= num;
+// // console.log(num);
+
+// num +=22;
+// console.log(num);
+// num *=33;
+// console.log(num);
+// num/=44;
+// console.log(num);
+// num -=55;
+// console.log(num);
+
+
+
+
+//Функції:
+
+function sayHello() { /// Function declaration
+    let userName1='NoName'
+    console.log('Привіт, юзере!')
+}
+
+sayHello();
+
+
+
+
+
+function goodWeather() {
+    alert('Сьогодні чудова погода');
+}
+
+goodWeather();
